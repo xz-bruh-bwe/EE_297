8c2b088e14c603fd9b2979ba6139bc0a4636a1fd
xz-bruh-bwe <135488759+xz-bruh-bwe@users.noreply.github.com>
2025-09-14 22:40:51 -0700

    Adding activation script

--- ML_PATH_EE297/EE297_env/zTransfer/run_EE297_jupyter.ps1
+++ ML_PATH_EE297/EE297_env/zTransfer/run_EE297_jupyter.ps1
@@ -1,7 +1,11 @@
-cd Desktop
-cd .\EE_297_Repo\EE_297
+cd "$HOME\Desktop\EE_297_Repo\EE_297"
 git pull
-cd .\ML_PATH_EE297
-.\EE297_env\Scripts\activate
-cd .\EE297_env
+
+# Activate your environment
+conda activate cuda_test
+
+# Change to working directory
+cd "$HOME\Desktop\EE_297_Repo\EE_297\ML_PATH_EE297\EE297_env"
+
+# Launch Jupyter Notebook
 jupyter notebook
