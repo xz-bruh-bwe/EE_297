8932de14106e3d2c0ed65814097a3b97b1395db8
Baron Eiley <Bweiley@cpp,edu>
2025-09-29 00:12:40 -0700

    Editting the code so that it is able to run on cpu only systems, uncomment certain code to run on GPU systems

---
+++ ML_PATH_EE297/EE297_env/zTransfer/.ipynb_checkpoints/run_EE297_jupyter-checkpoint.ps1
@@ -0,0 +1,11 @@
+cd "$HOME\Desktop\EE_297_Repo\EE_297"
+git pull
+
+# Activate your environment
+conda activate cuda_test
+
+# Change to working directory
+cd "$HOME\Desktop\EE_297_Repo\EE_297\ML_PATH_EE297\EE297_env"
+
+# Launch Jupyter Notebook
+jupyter notebook
